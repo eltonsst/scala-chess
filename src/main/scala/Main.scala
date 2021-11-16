b7b03a1f78fb1cfd009ac45145cd959f4bb57b23
eltonsst <elton.stafa@moneyfarm.com>
2021-11-16 16:59:42 +0100

    handled check

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,25 @@
 import com.whitehatgaming.UserInput
 import com.whitehatgaming.UserInputFile
+import Chess._
+
+def readMoves(
+    userInput: UserInputFile,
+    moves: List[((Int, Int), (Int, Int))]
+): List[((Int, Int), (Int, Int))] =
+  try
+    val move = userInput.nextMove()
+    val pos1 = (move(0) + 1, 8 - move(1)) // from e2 (file -> 5, rank -> 2)
+    val pos2 = (move(2) + 1, 8 - move(3)) // to e4 (file -> 5, rank -> 4)
+    readMoves(userInput, moves.appended(pos1, pos2))
+  catch
+    case _: NullPointerException =>
+      moves
+    case e: Exception =>
+      println(s"error while reading moves: ${e.getMessage}")
+      Nil
 
 @main def hello: Unit =
-  val init = initialState
-  print(init)
-  val userInput = new UserInputFile("./data/sample-moves-invalid.txt")
+  val userInput = new UserInputFile("./data/checkmate.txt")
   val moves = readMoves(userInput, Nil)
   println(moves)
-  play(moves, init)
+  play(moves, initialState)
